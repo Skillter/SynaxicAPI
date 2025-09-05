f7e22feeb07d5475bd377c34f84a344504ed8710
Skillter <57605954+Skillter@users.noreply.github.com>
2025-09-05 14:31:27 +0200

    Refactor tests to use Mockito and improve configuration

--- src/test/java/dev/skillter/synaxic/SynaxicApplicationTests.java
+++ src/test/java/dev/skillter/synaxic/SynaxicApplicationTests.java
@@ -1,15 +1,38 @@
 package dev.skillter.synaxic;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.command.CommandAsyncExecutor;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.ActiveProfiles;
 
-@Import(TestcontainersConfiguration.class)
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
+@ActiveProfiles("dev")
+@Import(SynaxicApplicationTests.TestConfig.class)
 class SynaxicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        @Primary
+        public RedissonClient redissonClient() {
+            Redisson mockRedisson = Mockito.mock(Redisson.class);
+            CommandAsyncExecutor mockExecutor = Mockito.mock(CommandAsyncExecutor.class);
+            when(mockRedisson.getCommandExecutor()).thenReturn(mockExecutor);
+            return mockRedisson;
+        }
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
