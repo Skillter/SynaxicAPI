211957e4347029e65e018fefd7c1ba54d44370a3
Skillter <57605954+Skillter@users.noreply.github.com>
2025-09-16 23:10:45 +0200

    small fix for self-signed certification

--- src/main/java/dev/skillter/synaxic/config/RateLimitConfig.java
+++ src/main/java/dev/skillter/synaxic/config/RateLimitConfig.java
@@ -9,7 +9,6 @@ import org.redisson.config.Config;
 import org.redisson.config.SslProvider;
 import org.redisson.config.SslVerificationMode;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -26,7 +25,7 @@ public class RateLimitConfig {
             @Value("${spring.data.redis.host}") String redisHost,
             @Value("${spring.data.redis.port}") int redisPort,
             @Value("${spring.data.redis.password}") String redisPassword,
-            @Value("classpath:truststore.p12") Resource trustStore
+            @Value("file:/app/truststore.p12") Resource trustStore
     ) throws Exception {
         Config config = new Config();
         String redisAddress = "rediss://" + redisHost + ":" + redisPort;
@@ -37,7 +36,7 @@ public class RateLimitConfig {
                 .setSslProvider(SslProvider.JDK)
                 .setSslTruststore(trustStore.getURL())
                 .setSslTruststorePassword("changeit")
-                .setSslVerificationMode(SslVerificationMode.NONE) // Correct method to disable hostname verification
+                .setSslVerificationMode(SslVerificationMode.NONE)
                 .setConnectionPoolSize(64)
                 .setConnectionMinimumIdleSize(24)
                 .setSubscriptionConnectionPoolSize(50)
