0302787f08f09d6c7a991703231df488ec3ad602
Skillter <57605954+Skillter@users.noreply.github.com>
2025-11-02 04:12:00 +0100

    fix cloudflare's script on production

--- src/main/java/dev/skillter/synaxic/config/SecurityConfig.java
+++ src/main/java/dev/skillter/synaxic/config/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig {
                     .contentSecurityPolicy(csp -> csp
                         .policyDirectives(
                             "default-src 'self'; " +
-                            "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+                            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com; " +
                             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
                             "font-src 'self' https://fonts.gstatic.com; " +
                             "img-src 'self' data: https:; " +
